692b2c70991c5aac9e178fe433fdd81a908641a5
frbarbre <fr.barbre@gmail.com>
2023-05-15 19:17:26 +0200

    Co-authored-by: Lean Kenneth Hansen (EAALKH) <eaalkh@students.eaaa.dk> Co-authored-by: littleoldmanboy <littleoldmanboy@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var navMenu = document.getElementById("nav-menu")
+var menuArrow = document.getElementById("menu-arrow")
+var navContent = document.getElementById("nav-content")
+
+function navToggle() {
+    if (navMenu.classList.contains("nav-neutral") || navMenu.classList.contains("nav-not-active")) {
+        navMenu.classList.remove("nav-not-active")
+        navMenu.classList.add("nav-active")
+        navMenu.classList.remove("nav-neutral")
+        menuArrow.classList.add("menu-arrow-active")
+        menuArrow.classList.remove("menu-arrow-not-active")
+        navContent.classList.add("nav-content-active")
+        navContent.classList.remove("nav-content-not-active")
+    }
+    else {
+        navMenu.classList.add("nav-not-active")
+        navMenu.classList.remove("nav-active")
+        menuArrow.classList.add("menu-arrow-not-active")
+        menuArrow.classList.remove("menu-arrow-active")
+        navContent.classList.remove("nav-content-active")
+        navContent.classList.add("nav-content-not-active")   
+    }
+}
