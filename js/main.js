b4270dcfc32bbf8d92a2be14a59fdbe60af5c420
frbarbre <fr.barbre@gmail.com>
2023-05-16 02:35:21 +0200

    ku ikk la vær sorry

--- js/main.js
+++ js/main.js
@@ -21,3 +21,17 @@ function navToggle() {
         navContent.classList.add("nav-content-not-active")   
     }
 }
+
+var mobileNav = document.getElementById("mobile-menu")
+
+function mobileNavToggle() {
+    if (mobileNav.classList.contains("mobile-not-active")) {
+        mobileNav.classList.add("mobile-active")
+        mobileNav.classList.remove("mobile-not-active")
+    }
+    else {
+        mobileNav.classList.remove("mobile-active")
+        mobileNav.classList.add("mobile-not-active")
+    }
+    navToggle()
+}
